a7b4f327e8ca73e859aece5ae70e8c58c50fce86
Ubuntu <ubuntu@ip-172-31-30-5.us-east-2.compute.internal>
2019-10-13 20:36:01 +0000

    update

--- src/main/groovy/com/example/demo/DemoApplication.groovy
+++ src/main/groovy/com/example/demo/DemoApplication.groovy
@@ -3,11 +3,12 @@ package com.example.demo
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@SpringBootApplication
+//@SpringBootApplication
 class DemoApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run(DemoApplication, args)
+		println 'Welcome to Groovy world ! Akshay Namdeo'
 	}
 
 }
